f5fff080042387ba205320910279182d04d89cce
Nicole <35063851+IishaWu@users.noreply.github.com>
2019-08-22 18:10:04 +0800

    Update registrar.py
    I think the line 129 would be revise to "app = aiosip.Application(loop=loop,mdialplan=Dialplan())"

--- examples/back_to_back/registrar.py
+++ examples/back_to_back/registrar.py
@@ -126,7 +126,7 @@ def main():
     args = parser.parse_args()
 
     loop = asyncio.get_event_loop()
-    app = aiosip.Application(loop=loop)
+    app = aiosip.Application(loop=loop,mdialplan=Dialplan())
 
     if args.protocol == 'udp':
         start(app, aiosip.UDP)
